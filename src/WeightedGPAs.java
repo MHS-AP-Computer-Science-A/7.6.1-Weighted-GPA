92759e0603c02a1c32b4345507e3bcd376b21618
foxrw1 <78601276+foxrw1@users.noreply.github.com>
2026-01-30 10:51:46 -0500

    Refine comments in WeightedGPAs main method
    Updated comments in the main method to clarify file operations and GPA calculations.

--- src/WeightedGPAs.java
+++ src/WeightedGPAs.java
@@ -2,10 +2,12 @@
 
 public class WeightedGPAs {
     public static void main(String[] args) throws Exception {
-        // Make the data file, then add your code here
+        // Open the file "gpa.txt". Then use the file data to 
+        // compute the unweighted and weighted gpas
 
 
-        
+        // Don't forget to round the final answers to 2 decimal points
     }
 }
+
 
